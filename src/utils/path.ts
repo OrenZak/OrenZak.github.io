f94d1840b3f4662126ae87dd140a4f366a1c1ac5
OrenZak <orenz@wix.com>
2022-04-11 08:23:04 +0300

    responsive

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -1,6 +1,18 @@
 //@ts-ignore
 import { isWebpSupported } from 'react-image-webp/dist/utils';
 
-export const webpPath = (imageName: string): string =>  window.location.origin + (isWebpSupported() ? `/${imageName}.webp` : '/webp-not-sup.png');
+export const webpPath = (imageName: string, hasPng: false): string =>  {
+  let imagePath = '/webp-not-sup.png';
+  if (isWebpSupported()) {
+    imagePath = `/${imageName}.webp`;
+  } else {
+    if (hasPng) {
+      imagePath = `/${imageName}.png`;
+    } else {
+      imagePath = '/webp-not-sup.png';
+    }
+  }
+  return window.location.origin + imagePath;
+};
 
 export const svgPath = (imageName: string): string =>  window.location.origin + `/${imageName}.svg`;
